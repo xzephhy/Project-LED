67acd892438f24f2c12ef5f7db72bc1d2f76ac54
xzephhy <chriskusema@hotmail.co.uk>
2018-03-27 12:18:11 +0100

    Update LED.py

--- LED.py
+++ LED.py
@@ -1,1 +1,35 @@
 
+import RPi.GPIO
+import time
+
+LED = ["Red", "Pink", "Green", "Purple"]
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(18,GPIO.OUT)
+    GPIO.setwarnings(false)
+    
+print ("LED On!")
+
+def led():
+    while True:
+        GPIO.output(18,GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(18,GPIO.LOW)
+        time.sleep(1)
+def destroy():
+    GPIO.output(LED,GPIO.LOW)
+    GPIO.cleanup()
+    
+setup()
+try:
+    led()
+except KeyboardInterrput:
+    CTRL+C
+    else:
+        Destroy()
+    
+
+   
+    
+   
